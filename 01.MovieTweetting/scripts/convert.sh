4d27548d9d88337a18a57addfbdef6cb43d89538
Roberto Turrin <roby@roberto.turrin>
2014-05-12 11:40:35 +0200

    Added script sh

---
+++ 01.MovieTweetting/scripts/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#$0: dataset_name
+#$1: converted dataset output dir
+#
+# example: sh convert.sh snapshots_10K /tmp
+
+if  [ $# -lt 2 ] ; then
+	echo 'missing input parameters'
+	exit
+fi
+
+DATASET_IN=../datasets/$1
+DATASET_OUT=$2
+
+groovy ConvertMovieTweetingsToCrowdRec $DATASET_IN $DATASET_OUT
