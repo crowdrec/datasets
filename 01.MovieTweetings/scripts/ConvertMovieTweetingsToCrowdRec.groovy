fd1fd0421b7c6999c8c05455d28a6b857fc31306
Viktor Gal <viktor.gal@maeth.com>
2014-07-31 12:21:54 +0800

    Fix issue #1 Convert both the 01.MovieTweetings converter script and the dataset itself to the new CrowdRec v4 format

--- 01.MovieTweetings/scripts/ConvertMovieTweetingsToCrowdRec.groovy
+++ 01.MovieTweetings/scripts/ConvertMovieTweetingsToCrowdRec.groovy
@@ -2,13 +2,13 @@ package dev.crowdrec.io
 
 /**
  * It converts the MovieTweetings dataset (https://github.com/sidooms/MovieTweetings) to the CrowdRec format:
- * 
+ *
  * == INPUT ==
  * An instance of the MovieTweetings dataset composed by 3 files: users.dat, movies.dat, ratings.dat
- * 
+ *
  * == OUTPUT
  * The dataset converted to CrowdRec format, composed by 2 files: entities.dat, relations.dat
- * 
+ *
  * entities.dat:
  * * etype: entity type (e.g., user, movie, actor,...)
  * * eid: entity unique identifier
@@ -21,40 +21,41 @@ package dev.crowdrec.io
  * * timestamp: the timestamp the relation was created (e.g., when a rating was given)
  * * properties: a map of properties (e.g., playtime)
  * * linkedEntities: a map of linked entities (e.g., subject/object of the relation,...)
- * 
+ *
  * == USAGE ==
  * groovy ConvertMovieTweetingsToCrowdRec INDIR OUTDIR
  * * INDIR: input directory containing user.dat, movies.dat, and ratings.dat
  * * OUTDIR: output directory that will contain entities.dat and relations.dat
- * 
+ *
  * @author Roberto Turrin
- * @version 1.0
+ * @author Viktor Gal
+ * @version 1.1
  */
 class ConvertMovieTweetingsToCrowdRec {
 	public static final String IN_USERS_FILENAME = 'users.dat'
 	public static final String IN_MOVIES_FILENAME = 'movies.dat'
 	public static final String IN_RATINGS_FILENAME = 'ratings.dat'
-	
+
 	public static final String OUT_ENTITIES_FILENAME = 'entities.dat'
 	public static final String OUT_RELATIONS_FILENAME = 'relations.dat'
-	
+
 	public static final String USER_ETYPE = 'user'
 	public static final String MOVIE_ETYPE = 'movie'
 	public static final String RATING_RTYPE = 'rating.explicit'
-	
+
 	private static final String IN_COL_SPLIT = '::'
 	private static final String OUT_COL_SPLIT = '\t'
 	private static final String PROP_SEP = '::'
 	private static final String CR = '\n'
-	
+
 	private static final String HEADER_ENTITIES = ['etype','eid','properties','linkedEntities'].join(OUT_COL_SPLIT)
 	private static final String HEADER_RELATIONS = ['rtype','rid','timestamp','properties','linkedEntities'].join(OUT_COL_SPLIT)
-	
-	private static final boolean DEBUG = false // if DEBUG it will not write to files but only to screen 
+
+	private static final boolean DEBUG = false // if DEBUG it will not write to files but only to screen
 	private static final boolean PRINT_HEADER = true // if enabled it print the header in files
-	
+
 	/**
-	 * 
+	 *
 	 * @param args
 	 * $0: input folder
 	 * $1: output folder (optional)
@@ -74,10 +75,10 @@ class ConvertMovieTweetingsToCrowdRec {
 		File users_in = new File(inpath + File.separator + IN_USERS_FILENAME)
 		File movies_in = new File(inpath + File.separator + IN_MOVIES_FILENAME)
 		File ratings_in = new File(inpath + File.separator + IN_RATINGS_FILENAME)
-		
+
 		File entities_out = new File(outpath + File.separator + OUT_ENTITIES_FILENAME)
 		File relations_out = new File(outpath + File.separator + OUT_RELATIONS_FILENAME)
-		
+
 		if ( !DEBUG && PRINT_HEADER ) {
 			entities_out.append(HEADER_ENTITIES + CR)
 			relations_out.append(HEADER_RELATIONS + CR)
@@ -90,16 +91,16 @@ class ConvertMovieTweetingsToCrowdRec {
 				addEntity(entities_out, USER_ETYPE, userid, [twitterid:twitter_userid], [])
 			}
 		}
-		
+
 		movies_in.eachLine { movie ->
 			def movie_els = movie.split(IN_COL_SPLIT)
 			if ( movie_els != null && movie_els.size() >= 1 ) {
 				def movieid = movie_els[0]
 				def title = movie_els.size() >= 2 ? movie_els[1] : ''
-				addEntity(entities_out, MOVIE_ETYPE, movieid, [title:quote(title)], [])
+				addEntity(entities_out, MOVIE_ETYPE, movieid, [title:title], [])
 			}
 		}
-		
+
 		ratings_in.eachLine { rating, rowindex ->
 			def rating_els = rating.split(IN_COL_SPLIT)
 			if ( rating_els != null && rating_els.size() >= 4 ) {
@@ -107,18 +108,18 @@ class ConvertMovieTweetingsToCrowdRec {
 				def movieid = rating_els[1]
 				def rating_value = rating_els[2]
 				def ts = rating_els[3]
-				addRelation(relations_out, RATING_RTYPE, rowindex, ts, [rating:rating_value], [subject:createEntityRef(USER_ETYPE,userid), object:createEntityRef(MOVIE_ETYPE,movieid) ])
+				addRelation(relations_out, RATING_RTYPE, rowindex, ts, [rating:rating_value.toInteger()], [subject:createEntityRef(USER_ETYPE,userid), object:createEntityRef(MOVIE_ETYPE,movieid) ])
 			}
 		}
 	}
-	
+
 	static createEntityRef(type, id) {
-		'('+type+':'+ id+')'
+		type+':'+ id
 	}
-	
+
 	static addEntity (file, etype, eid, propset, linkedentities) {
-		def props = flattenMap(propset)
-		def links = flattenMap(linkedentities)
+		def props = toJSON(propset)
+		def links = toJSON(linkedentities)
 		def output = [etype, eid, props, links].join(OUT_COL_SPLIT) + CR
 		if ( !DEBUG ) {
 			file.append(output)
@@ -126,10 +127,10 @@ class ConvertMovieTweetingsToCrowdRec {
 			print output
 		}
 	}
-	
+
 	static addRelation(file, rtype, rid, timestamp, propset, linkedentities) {
-		def props = flattenMap(propset)
-		def links = flattenMap(linkedentities)
+		def props = toJSON(propset)
+		def links = toJSON(linkedentities)
 		def output = [rtype, rid, timestamp, props, links].join(OUT_COL_SPLIT) + CR
 		if ( !DEBUG ) {
 			file.append(output)
@@ -137,16 +138,14 @@ class ConvertMovieTweetingsToCrowdRec {
 			print output
 		}
 	}
-	
-	static flattenMap(map) {
-		return map ?
-			map.collect { ptype, pname ->
-				ptype + '=' + pname + ''
-			}.join(PROP_SEP)
-			: ''
+
+	static toJSON(map) {
+		if (map.size() == 0) {
+			return ""
+		}
+
+		def json = new groovy.json.JsonBuilder(map)
+		return json.toString()
 	}
-	
-	static quote(str) {
-		'\'' + (str?str:'') + '\''
-	}
+
 }
